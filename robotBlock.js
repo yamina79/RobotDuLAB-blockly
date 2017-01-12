d652ca0eb6d1da67511db018af079ae2794b2705
yamina79 <jelassiyamina@gmail.com>
2017-01-12 15:50:04 +0100

    Update robotBlock.js

--- robotBlock.js
+++ robotBlock.js
@@ -329,6 +329,7 @@ Blockly.Blocks['blanc'] = {
     this.appendDummyInput()
         .appendField("blanc");
     this.setInputsInline(true);
+    this.setOutput(true, 'Number');
     this.setColour(210);
     this.setTooltip('');
       }
